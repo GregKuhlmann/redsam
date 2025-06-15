66d0c03e98eeb3df26b8e767c51657c7be377f02
Greg Kuhlmann <greg.kuhlmann@gmail.com>
2025-06-15 11:21:05 -0500

    kinda working

--- src/main.js
+++ src/main.js
@@ -3,18 +3,12 @@ import { Game } from "./scenes/Game.js";
 
 const config = {
   type: Phaser.WEBGL,
-  width: 240,
-  height: 160,
+  width: 320,
+  height: 224,
   pixelArt: true,
   parent: "game-container",
+  backgroundColor: "#71ddee",
   scene: [Game],
-  physics: {
-    default: "arcade",
-    arcade: {
-      gravity: { y: 300 },
-      debug: false,
-    },
-  },
   scale: {
     mode: Phaser.Scale.FIT,
     autoCenter: Phaser.Scale.CENTER_BOTH,
--- src/scenes/Game.js
+++ src/scenes/Game.js
@@ -1,93 +1,18 @@
 import * as Phaser from "phaser";
 
-import heroIdle from "/assets/metroidvania/herochar sprites(new)/herochar_idle_anim_strip_4.png";
-import background from "/assets/metroidvania/tiles and background_foreground (new)/background.png";
-import tileset from "/assets/metroidvania/tiles and background_foreground (new)/tileset.png";
-import coin from "/assets/metroidvania/miscellaneous sprites/coin_anim_strip_6.png";
-import coinPickup from "/assets/metroidvania/miscellaneous sprites/coin_pickup_anim_strip_6.png";
-import level1 from "/assets/maps/level1.json";
-import collect from "/assets/sounds/collect.mp3";
-import bounce from "/assets/sounds/bounce.mp3";
+import level1 from "/assets/maps/desert1.json";
+import tilesetHouse from "/assets/Ninja/Backgrounds/Tilesets/TilesetHouse.png";
+import tilesetNature from "/assets/Ninja/Backgrounds/Tilesets/TilesetNature.png";
+import tilesetWater from "/assets/Ninja/Backgrounds/Tilesets/TilesetWater.png";
+import tilesetItems from "/assets/Ninja/TilesetItems.png";
+import tilesetGUI from "/assets/Ninja/TilesetGUI.png";
+import spriteSheetRedNinja3 from "/assets/Ninja/Actor/Characters/RedNinja3/SpriteSheet.png";
+import spriteSheetDragon from "/assets/Ninja/Actor/Monsters/Dragon/SpriteSheet.png";
+import spriteSheetSpark from "/assets/Ninja/FX/Magic/Spark/SpriteSheet.png";
+import spriteSheetAura from "/assets/Ninja/FX/Magic/Aura/SpriteSheet.png";
+import spriteSheetChest from "/assets/Ninja/Items/Treasure/BigTreasureChest.png";
 
-const fog = `precision mediump float;
-#define MAX_MARCH_STEPS 16
-#define MARCH_STEP_SIZE 0.2
-
-#define NOISE_AMPLITUDE 0.75
-
-#define FBM_ITERATIONS 3
-#define FBM_AMPLITUDE_GAIN 0.8
-#define FBM_FREQUENCY_GAIN 1.9
-
-// 2*tan(radians(45)/2)
-#define FOV45 0.82842693331417825056778150945139
-
-uniform float time;
-uniform vec2 resolution;
-
-varying vec2 fragCoord;
-
-vec3 UvToWorld(vec2 uv) { return normalize(vec3((uv-0.5) * resolution.xy, -resolution.y / FOV45)); }
-vec3 Hash3( vec3 p ) { return fract(sin(vec3( dot(p,vec3(127.1,311.7,786.6)), dot(p,vec3(269.5,183.3,455.8)), dot(p,vec3(419.2,371.9,948.6))))*43758.5453); }
-float Voronoi(vec3 p)
-{
-	vec3 n = floor(p);
-	vec3 f = fract(p);
-
-	float shortestDistance = 1.0;
-	for (int x = -1; x < 1; x++) {
-		for (int y = -1; y < 1; y++) {
-			for (int z = -1; z < 1; z++) {
-				vec3 o = vec3(x,y,z);
-				vec3 r = (o - f) + 1.0 + sin(Hash3(n + o)*50.0)*0.2;
-				float d = dot(r,r);
-				if (d < shortestDistance) {
-					shortestDistance = d;
-				}
-			}
-		}
-	}
-	return shortestDistance;
-}
-
-float FractalVoronoi(vec3 p)
-{
-	float n = 0.0;
-	float f = 0.5, a = 0.5;
-	mat2 m = mat2(0.8, 0.6, -0.6, 0.8);
-	for (int i = 0; i < FBM_ITERATIONS; i++) {
-		n += Voronoi(p * f) * a;
-		f *= FBM_FREQUENCY_GAIN;
-		a *= FBM_AMPLITUDE_GAIN;
-		p.xy = m * p.xy;
-	}
-	return n;
-}
-
-vec2 March(vec3 origin, vec3 direction)
-{
-	float depth = MARCH_STEP_SIZE;
-	float d = 0.0;
-	for (int i = 0; i < MAX_MARCH_STEPS; i++) {
-		vec3 p = origin + direction * depth;
-		d = FractalVoronoi(p) * NOISE_AMPLITUDE;
-		depth += max(MARCH_STEP_SIZE, d);
-	}
-	return vec2(depth, d);
-}
-
-void main()
-{
-	vec2 res = resolution.xy;
-	vec2 uv = (fragCoord.xy / res);
-
-	vec3 direction = UvToWorld(uv);
-	vec3 origin = vec3(0.0, -time*0.2, 0.0);
-	vec2 data = March(origin, direction);
-    
-	gl_FragColor = vec4(1, 1, 1, 1) * data.y * data.x * 0.7;
-}
-`;
+const TILESIZE = 16;
 
 export class Game extends Phaser.Scene {
   constructor() {
@@ -95,134 +20,366 @@ export class Game extends Phaser.Scene {
   }
 
   preload() {
-    this.load.spritesheet("heroIdle", heroIdle, {
-      frameWidth: 16,
-      frameHeight: 16,
-    });
-    this.load.spritesheet("coin", coin, {
-      frameWidth: 8,
-      frameHeight: 8,
-    });
-    this.load.spritesheet("coinPickup", coinPickup, {
-      frameWidth: 8,
-      frameHeight: 8,
-    });
-    this.load.image("background", background);
-    this.load.image("tileset", tileset);
+    this.load.image("tilesetHouse", tilesetHouse);
+    this.load.image("tilesetNature", tilesetNature);
+    this.load.image("tilesetWater", tilesetWater);
+    this.load.image("tilesetItems", tilesetItems);
+    this.load.image("tilesetGUI", tilesetGUI);
     this.load.tilemapTiledJSON("level1", level1);
-    this.load.audio("collect", collect);
-    this.load.audio("bounce", bounce);
+    this.load.spritesheet("dragon", spriteSheetDragon, {
+      frameWidth: TILESIZE,
+      frameHeight: TILESIZE,
+    });
+    this.load.spritesheet("lolo", spriteSheetRedNinja3, {
+      frameWidth: TILESIZE,
+      frameHeight: TILESIZE,
+    });
+    this.load.spritesheet("items", tilesetItems, {
+      frameWidth: TILESIZE,
+      frameHeight: TILESIZE,
+    });
+    this.load.spritesheet("spark", spriteSheetSpark, {
+      frameWidth: 30,
+      frameHeight: 35,
+    });
+    this.load.spritesheet("aura", spriteSheetAura, {
+      frameWidth: 25,
+      frameHeight: 24,
+    });
+    this.load.spritesheet("chest", spriteSheetChest, {
+      frameWidth: TILESIZE,
+      frameHeight: TILESIZE,
+    });
   }
 
   create() {
-    const { height } = this.scale;
+    //const { width, height } = this.scale;
 
-    const level = this.make.tilemap({ key: "level1" });
-    const tileset = level.addTilesetImage("metroidvania", "tileset");
+    this.level = this.make.tilemap({ key: "level1" });
+    const tilesetHouse = this.level.addTilesetImage(
+      "TilesetHouse",
+      "tilesetHouse"
+    );
+    const tilesetNature = this.level.addTilesetImage(
+      "TilesetNature",
+      "tilesetNature"
+    );
+    const tilesetWater = this.level.addTilesetImage(
+      "TilesetWater",
+      "tilesetWater"
+    );
+    const tilesetGUI = this.level.addTilesetImage("TilesetGUI", "tilesetGUI");
+    this.tilesetItems = this.level.addTilesetImage(
+      "TilesetItems",
+      "tilesetItems"
+    );
+    const tilesets = [
+      tilesetHouse,
+      tilesetNature,
+      tilesetWater,
+      tilesetGUI,
+      this.tilesetItems,
+    ];
 
-    this.add
-      .tileSprite(0, 0, level.widthInPixels, height, "background")
-      .setOrigin(0)
-      .setScrollFactor(0.3, 0); // Parallax at 0.3
-    level.createLayer("mountains", tileset, 0, 0).setScrollFactor(0.6, 0); // Parallax at 0.6
-
-    const baseShader = new Phaser.Display.BaseShader("BufferShader", fog);
-    this.add
-      .shader(baseShader, 0, 0, level.widthInPixels, height)
-      .setOrigin(0)
-      .setScrollFactor(0.5);
-
-    const ground = level
-      .createLayer("ground", tileset, 0, 0)
-      .setCollisionBetween(1, 1000); // Choose which tile IDs collide
-
-    this.hero = this.physics.add
-      .sprite(16, height - 16, "heroIdle")
-      .setOrigin(0.5, 1)
-      .setBounce(0)
-      .setCollideWorldBounds(true);
-
-    // Set the hero's bounding box
-    this.hero.body.setSize(10, 16).setOffset(3, 0);
+    this.level.createLayer("LayerBackground", tilesets, 0, 0);
+    this.level.createLayer("LayerDecorations", tilesets, 0, 0);
+    this.level
+      .createLayer("LayerObstacles", tilesets, 0, 0)
+      .setCollisionBetween(1, 1000);
 
     this.anims.create({
-      key: "idle",
-      frames: this.anims.generateFrameNumbers("heroIdle", { start: 0, end: 3 }),
-      frameRate: 10,
-      repeat: -1,
-    });
-    this.hero.play("idle");
-    this.physics.add.collider(this.hero, ground);
-
-    // Coins
-    this.anims.create({
-      key: "coinSpin",
-      frames: this.anims.generateFrameNumbers("coin", { start: 0, end: 5 }),
-      frameRate: 10,
-      repeat: -1,
-    });
-    this.anims.create({
-      key: "coinPickup",
-      frames: this.anims.generateFrameNumbers("coinPickup", {
-        start: 0,
-        end: 5,
+      key: "lolo-idle-down",
+      frames: this.anims.generateFrameNumbers("lolo", {
+        frames: [0],
       }),
-      frameRate: 20,
+      frameRate: 10,
+      repeat: -1,
     });
-    this.coins = this.physics.add.staticGroup();
-    level.getObjectLayer("items").objects.forEach((item) => {
-      if (item.name !== "coin") return;
-      this.coins.create(item.x, item.y, "coin").setOrigin(0).play("coinSpin");
+    this.anims.create({
+      key: "lolo-walk-down",
+      frames: this.anims.generateFrameNumbers("lolo", {
+        frames: [0, 4, 8, 12],
+      }),
+      frameRate: 10,
+      repeat: -1,
     });
-    this.physics.add.overlap(
-      this.hero,
-      this.coins,
-      this.collectCoin,
-      null,
-      this
-    );
+    this.anims.create({
+      key: "lolo-idle-up",
+      frames: this.anims.generateFrameNumbers("lolo", {
+        frames: [1],
+      }),
+      frameRate: 10,
+      repeat: -1,
+    });
+    this.anims.create({
+      key: "lolo-walk-up",
+      frames: this.anims.generateFrameNumbers("lolo", {
+        frames: [1, 5, 9, 13],
+      }),
+      frameRate: 10,
+      repeat: -1,
+    });
+    this.anims.create({
+      key: "lolo-idle-left",
+      frames: this.anims.generateFrameNumbers("lolo", {
+        frames: [2],
+      }),
+      frameRate: 10,
+      repeat: -1,
+    });
+    this.anims.create({
+      key: "lolo-walk-left",
+      frames: this.anims.generateFrameNumbers("lolo", {
+        frames: [2, 6, 10, 14],
+      }),
+      frameRate: 10,
+      repeat: -1,
+    });
+    this.anims.create({
+      key: "lolo-idle-right",
+      frames: this.anims.generateFrameNumbers("lolo", {
+        frames: [3],
+      }),
+      frameRate: 10,
+      repeat: -1,
+    });
+    this.anims.create({
+      key: "lolo-walk-right",
+      frames: this.anims.generateFrameNumbers("lolo", {
+        frames: [3, 7, 11, 15],
+      }),
+      frameRate: 10,
+      repeat: -1,
+    });
+    this.anims.create({
+      key: "dragon-idle",
+      frames: this.anims.generateFrameNumbers("dragon", {
+        frames: [0, 4, 8, 12, 2, 6, 10, 14],
+      }),
+      frameRate: 5,
+      repeat: -1,
+    });
+    this.anims.create({
+      key: "spark",
+      frames: this.anims.generateFrameNumbers("spark", {
+        start: 0,
+        end: 8,
+      }),
+      frameRate: 10,
+      repeat: -1,
+    });
+    this.anims.create({
+      key: "aura",
+      frames: this.anims.generateFrameNumbers("aura", {
+        start: 0,
+        end: 4,
+      }),
+      frameRate: 10,
+      repeat: -1,
+    });
+    this.anims.create({
+      key: "chest-closed",
+      frames: this.anims.generateFrameNumbers("chest", {
+        start: 0,
+        end: 0,
+      }),
+      frameRate: 10,
+    });
+    this.anims.create({
+      key: "chest-open",
+      frames: this.anims.generateFrameNumbers("chest", {
+        start: 1,
+        end: 1,
+      }),
+      frameRate: 10,
+    });
 
-    // Set up camera
-    this.cameras.main.setBounds(
-      0,
-      0,
-      level.widthInPixels,
-      level.heightInPixels
-    );
-    this.cameras.main.startFollow(this.hero, true, 0.1, 0.1);
+    this.lolo = this.add.sprite(-TILESIZE, -TILESIZE, "lolo").setOrigin(0);
+    this.lolo.moving = false;
+    this.lolo.direction = "down";
+    this.lolo.chargeTween = null;
+    this.lolo.aura = null;
+    this.crystals = [];
+    this.crystalsRemaining = 0;
+    this.chest = null;
+    this.door = null;
+    this.dragons = [];
 
-    // Set up physics
-    this.physics.world.setBounds(
-      0,
-      0,
-      level.widthInPixels,
-      level.heightInPixels
-    );
-
+    // iterate over the key, value pairs in the tilesetItems.tileProperties
+    this.level.getLayer("LayerItems").data.forEach((row) => {
+      row.forEach((tile) => {
+        if (tile.index !== -1) {
+          const name = this.tilesetItems.getTileProperties(tile.index)?.name;
+          if (name === "lolo") {
+            this.lolo.setPosition(tile.x * TILESIZE, tile.y * TILESIZE);
+            this.lolo.aura = this.add
+              .sprite(tile.x * TILESIZE, tile.y * TILESIZE, "aura")
+              .setScale(16 / 25)
+              .setOrigin(0)
+              .setAlpha(0)
+              .play("aura");
+          } else if (name === "dragon") {
+            const dragon = this.add
+              .sprite(tile.x * TILESIZE, tile.y * TILESIZE, "dragon")
+              .setOrigin(0)
+              .play("dragon-idle");
+            this.dragons.push({ x: tile.x, y: tile.y, dragon });
+          } else if (name === "door") {
+            const door = this.add
+              .sprite(tile.x * TILESIZE, tile.y * TILESIZE, "items")
+              .setFrame(tile.index - this.tilesetItems.firstgid)
+              .setOrigin(0);
+            this.door = { x: tile.x, y: tile.y, door, open: false };
+          } else if (name === "crystal") {
+            const crystal = this.add
+              .sprite(tile.x * TILESIZE, tile.y * TILESIZE, "items")
+              .setFrame(tile.index - this.tilesetItems.firstgid)
+              .setOrigin(0);
+            const spark = this.add
+              .sprite(tile.x * TILESIZE, tile.y * TILESIZE, "spark")
+              .setScale(16 / 35)
+              .setOrigin(0)
+              .play({
+                key: "spark",
+                repeatDelay: Phaser.Math.Between(500, 1500),
+              });
+            this.crystals.push({ x: tile.x, y: tile.y, crystal, spark });
+            this.crystalsRemaining++;
+          } else if (name === "chest") {
+            const chest = this.add
+              .sprite(tile.x * TILESIZE, tile.y * TILESIZE, "chest")
+              .setOrigin(0)
+              .play("chest-closed");
+            const spark = this.add
+              .sprite(tile.x * TILESIZE, tile.y * TILESIZE, "spark")
+              .setScale(16 / 35)
+              .setOrigin(0)
+              .setVisible(false)
+              .play("spark");
+            this.chest = { x: tile.x, y: tile.y, chest, spark, open: false };
+          } else {
+            console.log(
+              `Unknown tile at (${tile.x}, ${tile.y}) with index ${tile.index} and name ${name}`
+            );
+          }
+        }
+      });
+    });
+    this.lolo.setDepth(1000);
+    this.lolo.aura.setDepth(1001);
     this.cursors = this.input.keyboard.createCursorKeys();
+    this.input.keyboard.on("keydown-SPACE", () => {
+      if (this.lolo.chargeTween) return;
+      this.lolo.chargeTween = this.tweens.add({
+        targets: this.lolo.aura,
+        alpha: 1,
+        duration: 500,
+        ease: "Power2",
+      });
+    });
+    this.input.keyboard.on("keyup-SPACE", () => {
+      if (this.lolo.chargeTween) {
+        this.lolo.chargeTween.stop();
+        this.lolo.chargeTween = null;
+        this.lolo.aura.setAlpha(0);
+      }
+    });
   }
 
   update() {
     if (this.cursors.left.isDown) {
-      this.hero.setVelocityX(-100);
-      this.hero.flipX = true;
+      this.move(-1, 0, "left");
     } else if (this.cursors.right.isDown) {
-      this.hero.setVelocityX(100);
-      this.hero.flipX = false;
-    } else {
-      this.hero.setVelocityX(0);
-    }
-    if (this.cursors.up.isDown && this.hero.body.blocked.down) {
-      this.hero.setVelocityY(-160);
-      this.sound.play("bounce");
+      this.move(1, 0, "right");
+    } else if (this.cursors.up.isDown) {
+      this.move(0, -1, "up");
+    } else if (this.cursors.down.isDown) {
+      this.move(0, 1, "down");
+    } else if (!this.lolo.moving) {
+      this.lolo.play(`lolo-idle-${this.lolo.direction}`);
     }
   }
 
-  collectCoin(hero, coin) {
-    coin.disableBody(false, false);
-    this.sound.play("collect");
-    coin.play("coinPickup", true).once("animationcomplete", () => {
-      coin.destroy();
+  move(dx, dy, direction) {
+    if (this.lolo.moving) return;
+    this.lolo.direction = direction;
+    this.lolo.play(`lolo-walk-${direction}`, true);
+
+    const nextX = this.lolo.x + dx * TILESIZE;
+    const nextY = this.lolo.y + dy * TILESIZE;
+
+    if (this.collisionAt(nextX, nextY)) return;
+
+    this.lolo.moving = true;
+    this.tweens.add({
+      targets: [this.lolo, this.lolo.aura],
+      x: nextX,
+      y: nextY,
+      duration: 200,
+      onComplete: () => {
+        this.lolo.moving = false;
+        // did lolo collect a crystal?
+        const tileX = Math.floor(nextX / TILESIZE);
+        const tileY = Math.floor(nextY / TILESIZE);
+        if (this.door.x == tileX && this.door.y == tileY) {
+          // freeze all tweens and animations
+          this.tweens.killAll();
+          this.anims.pauseAll();
+          this.cameras.main.fadeOut(1000, 0, 0, 0); // duration in ms, RGB fade color
+
+          this.cameras.main.once("camerafadeoutcomplete", () => {
+            this.anims.resumeAll();
+            this.scene.restart(); // or use this.scene.start('SceneName') to go to a different scene
+          });
+        }
+        if (this.chest.x == tileX && this.chest.y == tileY) {
+          this.chest.spark.setVisible(false);
+          this.door.open = true;
+          this.door.door.setVisible(false);
+          this.cameras.main.shake(200, 0.01);
+        }
+        this.crystals.forEach((crystal) => {
+          if (crystal.x == tileX && crystal.y == tileY) {
+            crystal.crystal.destroy();
+            crystal.spark.destroy();
+            this.crystalsRemaining--;
+            console.log(
+              `Collected crystal at (${tileX}, ${tileY}). Crystals remaining: ${this.crystalsRemaining}`
+            );
+            if (this.crystalsRemaining == 0) {
+              this.chest.open = true;
+              this.chest.chest.play("chest-open");
+              this.chest.spark.setVisible(true);
+            }
+          }
+        });
+      },
     });
+  }
+
+  collisionAt(x, y) {
+    const tileX = Math.floor(x / TILESIZE);
+    const tileY = Math.floor(y / TILESIZE);
+
+    if (this.door.x == tileX && this.door.y == tileY) {
+      return !this.door.open;
+    }
+
+    if (tileX < 4 || tileX > 14 || tileY < 1 || tileY > 12) return true;
+
+    let tile = this.level.getTileAt(tileX, tileY, true, "LayerObstacles");
+    if (tile && tile.index !== -1) {
+      return true;
+    }
+
+    if (this.chest.x == tileX && this.chest.y == tileY) {
+      return !this.chest.open;
+    }
+
+    if (this.dragons.some((dragon) => dragon.x == tileX && dragon.y == tileY)) {
+      return true;
+    }
+
+    return false;
   }
 }
